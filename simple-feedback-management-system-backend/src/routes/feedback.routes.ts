2db8be717b004399db01ddbbc6c07976886a9613
kana <patidarkanha44@email.com>
2024-06-15 12:38:16 +0530

    Initial commit

--- simple-feedback-management-system-backend/src/routes/feedback.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllFeedbacks, submitFeedback } from '../controllers/feedback.controller';
-
-const router = express.Router();
-
-router.get('/feedbacks', getAllFeedbacks);
-router.post('/feedbacks', submitFeedback);
-
-export default router;
